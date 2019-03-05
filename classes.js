f62b3dea0f35b221cb7657e8fc5a4597bce3fc76
beijilang <emolangshen@gmail.com>
2019-03-04 21:05:45 -0500

    fix merging problem

--- classes.js
+++ classes.js
@@ -174,43 +174,45 @@ class Comment{
     }
 }
 function newBook(author,book){
-    fakeBooks.push(book)
-    author.newBook(book)
+    fakeBooks.push(book);
+    author.newBook(book);
 }
 
 let numberOfUsers = 0;
 function userCreater(name,mailAddress,passWord){
-	const rawUser = new user(name, numberOfUsers)
-	rawUser.setMailAdd(mailAddress)
-	rawUser.setPassWord(passWord)
-	numberOfUsers += 1
+	const rawUser = new user(name, numberOfUsers);
+	rawUser.setMailAdd(mailAddress);
+	rawUser.setPassWord(passWord);
+	numberOfUsers += 1;
 	return rawUser
 }
 
 //fake data
-const fakeUser = []
-fakeUser.push(userCreater("Xie Wu","wuxie@gmail.com","123456"))
-fakeUser.push(userCreater("Cixin Liu","liucixin@gmail.com","123456"))
-fakeUser.push(userCreater("JK_Rowling","jkR@gmail.com","123456"))
-fakeUser.push(userCreater("admin","liucixin@gmail.com","admin"))
-fakeUser.push(userCreater("user","jkR@gmail.com","user"))
-fakeUser[0].setImage("img/XieWu.png")
-fakeUser[2].setImage("img/jk.jpg")
+let fakeUser = []
+fakeUser.push(userCreater("Xie Wu","wuxie@gmail.com","123456"));
+fakeUser.push(userCreater("Cixin Liu","liucixin@gmail.com","123456"));
+fakeUser.push(userCreater("JK_Rowling","jkR@gmail.com","123456"));
+fakeUser.push(userCreater("admin","liucixin@gmail.com","admin"));
+fakeUser.push(userCreater("user","jkR@gmail.com","user"));
+fakeUser[0].setImage("img/XieWu.png");
+fakeUser[1].setImage("img/dog.jpeg");
+fakeUser[2].setImage("img/jk.jpg");
+fakeUser[3].setImage("img/dog.jpeg");
+fakeUser[4].setImage("img/dog.jpeg");
+let fakeBooks = []
+newBook(fakeUser[2],new Book('Harry Potter', fakeUser[2],'1999/10/1','img/harryPotter.jpg','fantasy'));
+newBook(fakeUser[0],new Book('Time Raiders', fakeUser[0],'2002/4/5','img/TimeRaiders.jpg','fantasy'));
+newBook(fakeUser[1],new Book('Wandering Earth', fakeUser[1],'2008/8/8','img/WanderingEarth.jpg','Sci-fi'));
+newBook(fakeUser[1],new Book('ThreeBody Problem', fakeUser[1],'2010/5/3','img/threebody.jpg','Sci-fi'));
 
-const fakeBooks = []
-newBook(fakeUser[2],new Book('Harry Potter', fakeUser[2],'1999/10/1','img/harryPotter.jpg','fantasy'))
-newBook(fakeUser[0],new Book('Time Raiders', fakeUser[0],'2002/4/5','img/TimeRaiders.jpg','fantasy'))
-newBook(fakeUser[1],new Book('Wandering Earth', fakeUser[1],'2008/8/8','img/WanderingEarth.jpg','Sci-fi'))
-newBook(fakeUser[1],new Book('ThreeBody Problem', fakeUser[1],'2010/5/3','img/threebody.jpg','Sci-fi'))
-
-fakeBooks[1].addChapter(new Chapter(1,'1'))
-fakeBooks[1].addChapter(new Chapter(2,'2'))
-fakeBooks[1].addChapter(new Chapter(3,'3'))
-fakeBooks[1].addChapter(new Chapter(4,'4'))
-fakeBooks[1].addChapter(new Chapter(5,'5'))
-fakeBooks[1].addChapter(new Chapter(6,'6'))
-fakeBooks[1].addChapter(new Chapter(7,'7'))
-fakeBooks[1].newComment(new Comment(fakeUser[0],'this is a good book'))
+fakeBooks[1].addChapter(new Chapter(1,'1'));
+fakeBooks[1].addChapter(new Chapter(2,'2'));
+fakeBooks[1].addChapter(new Chapter(3,'3'));
+fakeBooks[1].addChapter(new Chapter(4,'4'));
+fakeBooks[1].addChapter(new Chapter(5,'5'));
+fakeBooks[1].addChapter(new Chapter(6,'6'));
+fakeBooks[1].addChapter(new Chapter(7,'7'));
+fakeBooks[1].newComment(new Comment(fakeUser[0],'this is a good book'));
 fakeBooks[1].setDescription('I begin tucking him into bed and he tells me, “Daddy check for monsters under my bed.” I look underneath for his amusement and see him, another him, under the bed, staring back at me quivering and whispering, “Daddy there’s somebody on my bed.”')
 
 fakeBooks[0].setRating(4);
@@ -230,8 +232,16 @@ let currentUserId = -1;
 
 //Following functions(help functions) are used to find books in the (fake)Books list. 
 //basic version
+function removeBook(title, inputList = fakeBooks){
+    return inputList.filter((fBook) => fBook.bookTitle !== title)
+}
+
+function removeUser(name, inputList = fakeUser){
+    return inputList.filter((fUser) => fUser.name !== name)
+}
+
 function searchBooksByTitle(title, inputList = fakeBooks){
-	return inputList.filter((fBook) => fBook.bookTitle == title)
+	return inputList.filter((fBook) => fBook.bookTitle === title)
 }
 
 //advanced version, the input can be anything: name, author or genre
@@ -240,14 +250,14 @@ function fuzzyBookSearch(input, inputList = fakeBooks){
 	//name search, similarity limit is .75
 	for(let index = 0; index<inputList.length; index++){
 		if(stringCompByLevenshteinDistance(input,inputList[index].getBookTitle()) > 0.75){
-			outputList.push(inputList[index])
+			outputList.push(inputList[index]);
 			console.log('Book found by title, similarity is '+stringCompByLevenshteinDistance(input,inputList[index].getBookTitle()))
 		}
 	}
 	//author search, similarity limit is .8
 	for(let index = 0; index<inputList.length; index++){
 		if(stringCompByLevenshteinDistance(input,inputList[index].getAuthor()) > 0.8){
-			outputList.push(inputList[index])
+			outputList.push(inputList[index]);
 			console.log('Book found by author, similarity is '+stringCompByLevenshteinDistance(input,inputList[index].getAuthor()))
 		}
 	}
@@ -255,7 +265,7 @@ function fuzzyBookSearch(input, inputList = fakeBooks){
 	//genre search, similarity limit is 1
 	for(let index = 0; index<inputList.length; index++){
 		if(stringCompByLevenshteinDistance(input,inputList[index].getGenre()) == 1){
-			outputList.push(inputList[index])
+			outputList.push(inputList[index]);
 			console.log('Book found by genre, similarity is '+stringCompByLevenshteinDistance(input,inputList[index].getGenre()))
 		}
 	}
@@ -272,7 +282,7 @@ function stringCompByLevenshteinDistance(s1,s2){
 		shorter = s1;
 	}
 	var longerLength = longer.length;
-	if (longerLength == 0) {
+	if (longerLength === 0) {
 		return 1.0;
 	}
 	return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
