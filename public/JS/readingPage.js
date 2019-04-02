9a07fd8ce546977248802ef5f27d1165601dabf2
Eric <chenghaowang03@gmail.com>
2019-04-02 19:46:38 -0400

    reading page get data from mongodb, and buttons

--- public/JS/readingPage.js
+++ public/JS/readingPage.js
@@ -3,10 +3,9 @@ const log = console.log;
 //get the id of book and chapter from somewhere
 const path = window.location.pathname.split("/")
 let book_id = path[1];
-log(book_id)
 let chapter_index = parseInt(path[2]);
-log(chapter_index)
-//
+log(book_id);
+log(chapter_index);
 
 function getChaptersReading(book_id){
     let url = '/db/reading/'+book_id;
@@ -28,7 +27,7 @@ function updateReadingPage(chapters){
     let nameBox = document.getElementById('chapterNameBox');
     let contentBox = document.getElementById('mainText');
 
-    log(chapters);
+    //log(chapters);
     chapters.then((array)=>{
         nameBox.innerHTML = array[chapter_index].chapterTitle;
         contentBox.innerHTML = array[chapter_index].content;
@@ -36,34 +35,36 @@ function updateReadingPage(chapters){
 
 }
 
-let chapters = getChaptersReading(book_id);
-updateReadingPage(chapters);
 
-
-// those two functions will be assign to the onclick of the buttons on 4 corner of the reading pages
-function showNextChapter(){
-    chapter_index += 1;
-    updateReadingPage(chapters);
-}
-
-function showLastChapter(){
-    chapter_index -= 1;
-    updateReadingPage(chapters);
-}
-
-function activateButtons(){
+function activateButtons(chapters) {
     let upperLeftButton = document.getElementById('upperLeftButton');
     let upperRightButton = document.getElementById('upperRightButton');
     let lowerLeftButton = document.getElementById('lowerLeftButton');
     let lowerRightButton = document.getElementById('lowerRightButton');
 
-    upperLeftButton.onclick = showLastChapter;
-    lowerRightButton.onclick = showNextChapter;
-    upperRightButton.onclick = showNextChapter;
-    lowerLeftButton.onclick = showLastChapter;
-}
+    let chapter_num_totally = 0;
+    log(chapters);
+    chapters.then((array)=>{
+        chapter_num_totally = array.length;
+        resolve(chapter_num_totally);
+    });
 
-//activate the buttons' functions
-activateButtons();
+
+    if(chapter_index<chapter_num_totally) {
+        //TODO
+        log('can go next')
+        upperRightButton.children[0].href = "/" + book_id + "/" + (chapter_index + 1);
+        lowerRightButton.children[0].href = "/" + book_id + "/" + (chapter_index + 1);
+    }
+    if (chapter_index > 0) {
+        log('can go back')
+        upperLeftButton.children[0].href = "/" + book_id + "/" + (chapter_index - 1);
+        lowerLeftButton.children[0].href = "/" + book_id + "/" + (chapter_index - 1);
+    }
+}
+//call of functions
+let chapters = getChaptersReading(book_id);
+updateReadingPage(chapters);
+activateButtons(chapters);
 
 log("end of hard code")
